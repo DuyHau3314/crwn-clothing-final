041534b40a175857503acdba1a711329afd16608
Hau Trinh <congtuzxc@gmail.com>
2021-11-20 11:51:23 +0700

    update add linkUrl and routing-v6

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
-
+import { useLocation } from 'react-router-dom';
 import Directory from '../../components/directory/directory.component';
-
 import './homepage.styles.scss';
 
-const HomePage = () => (
-  <div className='homepage'>
+
+
+const HomePage = (props) => {
+  const a = useLocation();
+  console.log(a);
+  return (
+<div className='homepage'>
     <Directory />
   </div>
-);
+  )
+  
+  };
 
 export default HomePage;
+
+// useLocation - {
+//     "pathname": "/",
+//     "search": "",
+//     "hash": "",
+//     "state": null,
+//     "key": "default"
+// }
