59ace69e205dea1cd22370845dfd86594b4cb0e4
Hau Trinh <congtuzxc@gmail.com>
2021-11-21 08:54:38 +0700

    done sign in handleSubmut

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -45,7 +45,7 @@ class SignIn extends React.Component {
 
                 <div className="buttons">
                 <CustomButton type="submit">Submit Form</CustomButton>
-                <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+                <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
                 </div>
             </form>
         </div>;
